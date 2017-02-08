4ba1c7e0c1d2363ee891eff9c391c4d0f1d24a74
Petermuturi <pmuturi65@gmail.com>
2017-02-08 17:25:27 +0300

    fetching user location

--- components/GeoLocation.js
+++ components/GeoLocation.js
@@ -1,18 +1,47 @@
-import React from 'react';
-import { Text } from 'react-native';
+'use strict';
 
-export default class GeoLocation extends React.Component {
-  static propTypes = {
-    name: React.PropTypes.string,
-  };
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-  constructor(props) {
+
+export default class GeoLocation extends Component {
+	constructor(props) {
     super(props);
+    this.state = {
+    		userPosition: 'loading...',
+    		geo: false
+    }
+  	watchID: (null: ?number)
   }
+
+
+	componentDidMount() {
+		 this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lastPosition = JSON.stringify(position);
+      this.setState({userPosition: lastPosition, geo: true});
+    });
+
+	}
+
+	componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
 
   render() {
     return (
-      <Text>Hello World</Text>
+      <View>	
+      	<Text>
+          <Text style={styles.title}>User's position:  </Text>
+          {this.state.userPosition}
+        </Text>
+      </View>
     );
   }
 }
+
+var styles = StyleSheet.create({
+  title: {
+    fontWeight: '500',
+  },
+});
